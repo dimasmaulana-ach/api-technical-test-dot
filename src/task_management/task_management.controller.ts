7dd0e6b14bc5c993d819f8c50323a068b9db413f
Dimas Maulana Ahmad <97804178+dimasmaulana-ach@users.noreply.github.com>
2025-03-28 07:27:56 +0700

    Update task_management.controller.ts

--- src/task_management/task_management.controller.ts
+++ src/task_management/task_management.controller.ts
@@ -6,31 +6,40 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TaskManagementService } from './task_management.service';
 import { CreateTaskManagementDto } from './dto/create-task_management.dto';
 import { UpdateTaskManagementDto } from './dto/update-task_management.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('task-management')
 export class TaskManagementController {
   constructor(private readonly taskManagementService: TaskManagementService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
+  @UseGuards(AuthGuard)
   create(@Body() createTaskManagementDto: CreateTaskManagementDto) {
     return this.taskManagementService.create(createTaskManagementDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.taskManagementService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.taskManagementService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(
     @Param('id') id: string,
     @Body() updateTaskManagementDto: UpdateTaskManagementDto,
@@ -39,6 +48,7 @@ export class TaskManagementController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.taskManagementService.remove(id);
   }
